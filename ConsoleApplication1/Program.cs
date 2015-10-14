403e7cedc9fa371dbd6b4cf6df3a968f07c4369b
itdoesnmakesense <juan.j.rodriguez2@gmail.com>
2015-10-14 08:16:12 -0500

    print with an "a" or "an"

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,8 +10,29 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("hello There!What's your name?");
-            System.Console.ReadLine();
+            System.Console.WriteLine("Hey There! What's your name?");
+           string name = System.Console.ReadLine();
+           System.Console.WriteLine("Hi, " + name);
+          
+           for (int i = 0; i < name.Length; i++)
+           {
+               bool article = "halfnorsemix".IndexOf(name[i].ToString(), StringComparison.InvariantCultureIgnoreCase) >= 0;
+                if(article)
+                {
+                   System.Console.WriteLine("Give me an ... " + name[i]);
+                }
+                else
+                {
+                    System.Console.WriteLine("Give me a ..." + name[i]);
+                }
+                 
+            }
+            System.Console.WriteLine( name + " is awesome!");
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadKey();
+           }
+          
+      
         }
     }
-}
+
